593644e9e61364ed3c5790733703a3ad72d0458c
erick <erickrs2esuarez@gmail.com>
2025-09-05 13:31:51 -0300

    bboard contract

---
+++ bulletin-board/src/index.ts
@@ -0,0 +1,2 @@
+export * as Board from "./managed/board/contract/index.cjs";
+export * from "./witnesses";
---
+++ bulletin-board/src/witnesses.ts
@@ -0,0 +1,19 @@
+import { Ledger } from "./managed/board/contract/index.cjs";
+import { WitnessContext } from "@midnight-ntwrk/compact-runtime";
+
+export type BBoardPrivateState = {
+  readonly secretKey: Uint8Array;
+};
+
+export const createBBoardPrivateState = (secretKey: Uint8Array) => ({
+  secretKey,
+});
+
+export const witnesses = {
+  localSecretKey: ({
+    privateState,
+  }: WitnessContext<Ledger, BBoardPrivateState>): [
+    BBoardPrivateState,
+    Uint8Array,
+  ] => [privateState, privateState.secretKey],
+};
