4cf1397645560895d0d6faed9e1329ab36a76ce5
erick <erickrs2esuarez@gmail.com>
2025-12-17 20:11:03 -0300

    cli logic complete for counter

--- counter-cli/src/config.ts
+++ counter-cli/src/config.ts
@@ -20,7 +20,7 @@ export class StandaloneConfig implements Config {
   logDir = path.resolve(currentDir, '..', 'logs', 'standalone', `${new Date().toISOString()}.log`);
   indexer = 'http://127.0.0.1:8088/api/v3/graphql';
   indexerWS = 'ws://127.0.0.1:8088/api/v3/graphql/ws';
-  node = 'http://127.0.0.1:9944';
+  node = 'ws://127.0.0.1:9944';
   proofServer = 'http://127.0.0.1:6300';
   networkId = 'undeployed';
 }
@@ -28,7 +28,7 @@ export class StandaloneConfig implements Config {
 export class PreviewConfig implements Config {
   logDir = path.resolve(currentDir, '..', 'logs', 'preview', `${new Date().toISOString()}.log`);
   indexer = 'https://indexer.preview.midnight.network/api/v3/graphql';
-  indexerWS = 'wss://indexer.preview.midnight.network/api/v3/graphql/ws';
+  indexerWS = 'ws://indexer.preview.midnight.network/api/v3/graphql/ws';
   node = 'wss://rpc.preview.midnight.network';
   proofServer = 'http://127.0.0.1:6300';
  networkId = 'preview';
