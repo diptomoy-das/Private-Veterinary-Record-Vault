f7e86f32ca122149867fe75b1f85a2a96d75d289
erick <erickrs2esuarez@gmail.com>
2026-01-01 21:41:54 -0300

    wallet testing intents

--- counter-cli/src/test/wallet.test.ts
+++ counter-cli/src/test/wallet.test.ts
@@ -157,4 +157,95 @@ describe('API', () => {
         .pipe(Rx.filter((s) => s.unshielded.availableCoins.some((c) => c.utxo.value === tokenValue(1n)))),
     );
   });
+
+  it('allows to balance and submit an arbitrary shielded transaction', async () => {
+    const transfer = {
+      type: ledger.shieldedToken().raw,
+      amount: tokenValue(1n),
+    };
+
+    const coin = ledger.createShieldedCoinInfo(transfer.type, transfer.amount);
+    const output = ledger.ZswapOutput.new(
+      coin,
+      0,
+      wallet.shieldedSecretKeys.coinPublicKey,
+      wallet.shieldedSecretKeys.encryptionPublicKey,
+    );
+
+    const outputOffer = ledger.ZswapOffer.fromOutput(output, transfer.type, transfer.amount);
+
+    const arbitraryTx = ledger.Transaction.fromParts(configuration.networkId, outputOffer);
+
+    const provenArbitraryTx = await wallet.wallet.finalizeTransaction({
+      type: 'TransactionToProve',
+      transaction: arbitraryTx,
+    });
+
+    const balancedTx = await wallet.wallet.balanceTransaction(
+      wallet.shieldedSecretKeys,
+      wallet.dustSecretKey,
+      provenArbitraryTx,
+      new Date(Date.now() + 30 * 60 * 1000),
+    );
+
+    const finalizedTx = await wallet.wallet.finalizeTransaction(balancedTx);
+    const submittedTxHash = await wallet.wallet.submitTransaction(finalizedTx);
+    logger.info({
+      section: 'Submitted Transaction Hash',
+      submittedTxHash,
+    });
+
+    await Rx.firstValueFrom(
+      wallet.wallet
+        .state()
+        .pipe(Rx.filter((s) => s.shielded.availableCoins.some((c) => c.coin.value === tokenValue(1n)))),
+    );
+  });
+
+  it('allows to balance and submit an arbitrary unshielded transaction', async () => {
+    const outputs = [
+      {
+        type: ledger.unshieldedToken().raw,
+        value: tokenValue(1n),
+        owner: wallet.unshieldedKeystore.getAddress(),
+      },
+    ];
+
+    const intent = ledger.Intent.new(new Date(Date.now() + 30 * 60 * 1000));
+    intent.guaranteedUnshieldedOffer = ledger.UnshieldedOffer.new([], outputs, []);
+
+    const arbitraryTx = ledger.Transaction.fromParts(configuration.networkId, undefined, undefined, intent);
+
+    const recipe = await wallet.wallet.balanceTransaction(
+      wallet.shieldedSecretKeys,
+      wallet.dustSecretKey,
+      arbitraryTx,
+      new Date(Date.now() + 30 * 60 * 1000),
+    );
+
+    if (recipe.type !== 'TransactionToProve') {
+      throw new Error('Expected a transaction to prove');
+    }
+
+    const signedTx = await wallet.wallet.signTransaction(recipe.transaction, (payload) =>
+      wallet.unshieldedKeystore.signData(payload),
+    );
+
+    const finalizedTx = await wallet.wallet.finalizeTransaction({
+      ...recipe,
+      transaction: signedTx,
+    });
+
+    const submittedTxHash = await wallet.wallet.submitTransaction(finalizedTx);
+    logger.info({
+      section: 'Submitted Transaction Hash',
+      submittedTxHash,
+    });
+
+    await Rx.firstValueFrom(
+      wallet.wallet
+        .state()
+        .pipe(Rx.filter((s) => s.unshielded.availableCoins.some((c) => c.utxo.value === tokenValue(1n)))),
+    );
+  });
 });
